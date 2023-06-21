8eb47342e1a41cb24e24706064e5f1ddfda7372f
Nick Nyanjui <nicknyanjui@gmail.com>
2023-06-22 02:02:34 +0300

    Delete home page, help page and settings page from api views && Add librarian page views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
-from api.v1.views.index import *
-from api.v1.views.books import *
-from api.v1.views.members import *
 from flask import Blueprint
 
 app_views = Blueprint('routes', __name__, url_prefix='/api/v1')
+
+from api.v1.views.librarians import *
+from api.v1.views.books import *
+from api.v1.views.members import *
--- api/v1/views/books.py
+++ api/v1/views/books.py
@@ -8,5 +8,5 @@ from api.v1.views import app_views
 @app_views.route('/books', methods=['GET'], strict_slashes=False)
 def books():
     """Returns books page"""
-    b = Book()
-    return jsonify(str(b))
+    books = Book.query.all()
+    return jsonify(books)
