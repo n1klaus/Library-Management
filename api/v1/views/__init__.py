199b72e9415c5ec6c2e2e8372a1f2388622d73d0
Nick Nyanjui <nicknyanjui@gmail.com>
2023-06-30 00:12:01 +0300

    Fix import module ordering to prevent import loop

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
-from api.v1.views.member import *
-from api.v1.views.book import *
-from api.v1.views.librarian import *
 from flask import Blueprint
 
 app_views = Blueprint('routes', __name__, url_prefix='/api/v1')
+
+from api.v1.views.member import *
+from api.v1.views.book import *
+from api.v1.views.librarian import *
