dbd4e66a8a451957768fbae09a92e6f4cda28500
Nick Nyanjui <nicknyanjui@gmail.com>
2023-06-29 23:56:07 +0300

    Update main app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Main Flask App"""
+
 from flask import Flask, render_template
 from models.base_model import sa
 from api.v1.views import app_views
@@ -14,28 +16,28 @@ with app.app_context():
     sa.create_all()
 
 
-@app_views.route('/', methods=['GET'])
-@app_views.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def home():
-    """Returns home page"""
+    """Renders home page"""
     return render_template('home.html')
 
 
-@app.route('/status', methods=['GET'])
+@app.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns status page"""
+    """Renders status page"""
     return render_template('status.html')
 
 
 @app.route('/help', methods=['GET'], strict_slashes=False)
 def help():
-    """Returns help page"""
+    """Renders help page"""
     return render_template('help.html')
 
 
 @app.route('/settings', methods=['GET'], strict_slashes=False)
 def settings():
-    """Returns settings page"""
+    """Renders settings page"""
     return render_template('settings.html')
 
 
